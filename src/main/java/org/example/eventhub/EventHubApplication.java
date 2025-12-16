acc99349d9207b403deec9db87e5feefcb8ab39f
sta66y <sta66y@yahoo.com>
2025-12-16 21:52:49 +0700

    init commit

---
+++ src/main/java/org/example/eventhub/EventHubApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventHubApplication.class, args);
+    }
+
+}
