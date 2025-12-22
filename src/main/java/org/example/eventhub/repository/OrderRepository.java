a489fb4e3d61b02478f7e5d1de7c85eb59e2ec3f
sta66y <sta66y@yahoo.com>
2025-12-22 15:27:47 +0700

    добавил запросы в репозитории

--- src/main/java/org/example/eventhub/repository/OrderRepository.java
+++ src/main/java/org/example/eventhub/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package org.example.eventhub.repository;
 
-import org.example.eventhub.entity.Event;
 import org.example.eventhub.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.user.id = :userId")
+    List<Order> findAllByUserId(@Param("userId") Long userId);
 }
--- src/main/java/org/example/eventhub/repository/TicketRepository.java
+++ src/main/java/org/example/eventhub/repository/TicketRepository.java
@@ -3,6 +3,10 @@ package org.example.eventhub.repository;
 import org.example.eventhub.entity.Event;
 import org.example.eventhub.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("SELECT COUNT(t) FROM Ticket t WHERE t.event = :event AND t.status IN ('PAID', 'RESERVED')")
+    int countActiveByEvent(@Param("event") Event event);
 }
