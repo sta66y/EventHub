767e83710312c167101d17c48a981e7fd23949ee
sta66y <sta66y@yahoo.com>
2025-12-22 15:29:00 +0700

    OrderController

---
+++ src/main/java/org/example/eventhub/controller/OrderController.java
@@ -0,0 +1,44 @@
+package org.example.eventhub.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.eventhub.dto.order.OrderCreateRequest;
+import org.example.eventhub.dto.order.OrderResponseLong;
+import org.example.eventhub.dto.order.OrderResponseShort;
+import org.example.eventhub.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    private final OrderService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public OrderResponseLong createOrder(@Valid @RequestBody OrderCreateRequest dto, @RequestParam Long userId) {
+        return service.createOrder(dto, userId);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public OrderResponseLong getOrderById(@PathVariable Long id) {
+        return service.getOrderById(id);
+    }
+
+    @GetMapping("/my")
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderResponseShort> getAllOrders(@RequestParam Long userId) {
+        return service.getAllOrders(userId);
+    }
+
+    @PostMapping("/{id}/cancel")
+    @ResponseStatus(HttpStatus.OK)
+    public OrderResponseLong cancelOrder(@PathVariable Long id) { //TODO короч надо придумать как точечно возвращать билеты
+        return service.cancelOrder(id);
+    }
+}
