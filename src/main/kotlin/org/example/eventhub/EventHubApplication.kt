de62646eeabc5c926776f6f986a079588c6d6aff
sta66y <sta66y@yahoo.com>
2026-01-19 11:38:30 +0700

    точка входа в приложение теперь на котлине

--- src/main/kotlin/org/example/eventhub/EventHubApplication.kt
+++ src/main/kotlin/org/example/eventhub/EventHubApplication.kt
@@ -1,14 +1,13 @@
-package org.example.eventhub;
+package org.example.eventhub
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-public class EventHubApplication {
+class EventHubApplication
 
-    public static void main(String[] args) {
-        SpringApplication.run(EventHubApplication.class, args);
-    }
+fun main(args: Array<String>) {
+    runApplication<EventHubApplication>(*args)
 }
